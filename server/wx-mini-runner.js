a5bfa020ac93c2b6253a7ad9e3eabbc3b5b73de2
田放 <tianfanga@zbj.com>
2019-01-16 11:26:01 +0800

    wx-mini

--- server/wx-mini-runner.js
+++ server/wx-mini-runner.js
@@ -6,19 +6,22 @@ const utils = require("./utils/utils");
 const uuid = require('uuid');
 const co = require('co');
 let listJsonStr = "";
-writeWxJson("vicious-syndicate", "standard", "newest-list");
-writeWxJson("vicious-syndicate", "standard", "old-list");
-writeWxJson("vicious-syndicate", "wild", "list");
-writeWxJson("tempo-storm", "standard", "newest-list");
-writeWxJson("tempo-storm", "standard", "old-list");
-writeWxJson("tempo-storm", "wild", "list");
+// let deckJsonStr = "";
+co(function* () {
+  yield writeWxJson("vicious-syndicate", "standard", "newest-list");
+  yield writeWxJson("vicious-syndicate", "standard", "old-list");
+  yield writeWxJson("vicious-syndicate", "wild", "list");
+  yield writeWxJson("tempo-storm", "standard", "newest-list");
+  yield writeWxJson("tempo-storm", "standard", "old-list");
+  yield writeWxJson("tempo-storm", "wild", "list");
+});
 
 function writeWxJson(from, type, fileName) {
   const list = require(`../storage/${from}/${type}/report/${fileName}`);
-  co(function* () {
+  return co(function* () {
     for (let i = 0; i < list.length; i++) {
       let report = list[i];
-      if(!report.jumpUrl){
+      if (!report.jumpUrl) {
         let decks = require(`../storage/${from}/${type}/deck/${report.name}`);
         let occupations = Object.keys(decks);
         let jsonStr = "";
@@ -39,7 +42,8 @@ function writeWxJson(from, type, fileName) {
               deckFormat.cards = deck.cards;
               deckFormat.code = deck.code;
               deckFormat.time = report.time;
-              jsonStr += (JSON.stringify(deckFormat) + "\n")
+              jsonStr += (JSON.stringify(deckFormat) + "\n");
+              // deckJsonStr += (JSON.stringify(deckFormat) + "\n");
             }
           }
           yield utils.makeDirs(dirPath);
@@ -57,5 +61,6 @@ function writeWxJson(from, type, fileName) {
       listJsonStr += (JSON.stringify(listFormat) + "\n")
     }
     yield utils.writeFile(path.join(rootDir, "report.json"), listJsonStr);
+    // yield utils.writeFile(path.join(rootDir, "deck.json"), deckJsonStr);
   });
 }
