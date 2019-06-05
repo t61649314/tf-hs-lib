2fcece80b5ea13b80889428a613682b457c6db46
田放 <459126479@qq.com>
2019-06-05 09:49:09 +0800

    package.json

--- server/runner.js
+++ server/runner.js
@@ -4,15 +4,15 @@ const TeamRankstarSpider = require("./spider/TeamRankstarSpider");
 const HearthstoneTopDecksSpider = require("./spider/HearthstoneTopDecksSpider");
 const co = require('co');
 co(function* () {
-  const vs = new ViciousSyndicateSpider();
-  const ts = new TempoStormSpider();
-  const tr = new TeamRankstarSpider();
+  // const vs = new ViciousSyndicateSpider();
+  // const ts = new TempoStormSpider();
+  // const tr = new TeamRankstarSpider();
   const htd = new HearthstoneTopDecksSpider();
-  yield vs.runStandard();
-  yield vs.runWild();
-  yield ts.runStandard();
-  yield ts.runWild();
-  yield tr.run();
+  // yield vs.runStandard();
+  // yield vs.runWild();
+  // yield ts.runStandard();
+  // yield ts.runWild();
+  // yield tr.run();
   yield htd.run();
 });
 
