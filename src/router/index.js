ce7c6cca3d251fe80ccdf5b5f7c439766389babe
田放 <459126479@qq.com>
2019-09-22 17:08:23 +0800

    package.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
+import ReportList from '@/views/ReportList'
+import OccupationList from '@/views/OccupationList'
+import DeckList from '@/views/DeckList'
 
 
 Vue.use(Router)
@@ -11,6 +14,21 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/ReportList',
+      name: 'ReportList',
+      component: ReportList
+    },
+    {
+      path: '/OccupationList',
+      name: 'OccupationList',
+      component: OccupationList
+    },
+    {
+      path: '/DeckList',
+      name: 'DeckList',
+      component: DeckList
     }
   ]
 })
